0ace27e119a8b56a8d84e4288c125e8f45601ed2
Laro16 <luis21aro@gmail.com>
2025-09-26 13:02:50 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // --- AÑADE ESTA LÍNEA ---
-  base: '/<TInvitacion>/', 
+  base: '/<Invitacion>/', 
 })
+
 
